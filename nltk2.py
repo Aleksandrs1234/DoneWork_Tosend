f98d6b8d493f1a96b8de7970eb4664be2e748a5c
Aleksandrs1234 <114806211+Aleksandrs1234@users.noreply.github.com>
2024-11-21 15:38:05 +0200

    Update nltk2.py

--- nltk2.py
+++ nltk2.py
@@ -2,24 +2,21 @@ from langdetect import detect, detect_langs
 
 def detect_language(text):
     try:
-        # Detect the primary language of the text
         language = detect(text)
         
-        # Get probabilities of detected languages
         languages_with_probabilities = detect_langs(text)
         
         return language, languages_with_probabilities
     except Exception as e:
         return f"Error: {e}", []
 
-# Example texts
 texts = [
-    "Šodien ir saulaina diena.",  # Latvian
-    "Today is a sunny day.",      # English
-    "Сегодня солнечный день."     # Russian
+    "Šodien ir saulaina diena.",  
+    "Today is a sunny day.",      
+    "Сегодня солнечный день."     
 ]
 
-# Analyze each text
+
 for text in texts:
     lang, probabilities = detect_language(text)
     print(f"Text: {text}")
