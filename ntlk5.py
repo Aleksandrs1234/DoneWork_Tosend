8eba483778972a57d5e07a8fc5ba5fe3c2300580
Aleksandrs1234 <114806211+Aleksandrs1234@users.noreply.github.com>
2024-11-21 15:39:25 +0200

    Update ntlk5.py

--- ntlk5.py
+++ ntlk5.py
@@ -1,27 +1,20 @@
 import re
 
 def clean_text(raw_text):
-    # Noņem lietotājvārdus (@...)
     text = re.sub(r"@\w+", "", raw_text)
     
-    # Noņem URL
     text = re.sub(r"http\S+|www\S+|https\S+", "", text)
     
-    # Noņem liekos simbolus (emojis, !!!, ? u.c.)
     text = re.sub(r"[^\w\s]", "", text)
     
-    # Pārveido uz mazajiem burtiem
     text = text.lower()
     
-    # Noņem liekās atstarpes
     text = re.sub(r"\s+", " ", text).strip()
     
     return text
 
-# Neapstrādāts teksts
 raw_text = "@John: Šis ir lielisks produkts!!! Vai ne? 👏👏👏 http://example.com"
 
-# Tīrīšana un normalizēšana
 cleaned_text = clean_text(raw_text)
 
 # Rezultāts
